8e93732a3eec9db5a906a4a085f3af11d3695816
kamichidu <c.kamunagi@gmail.com>
2026-05-12 12:15:05 +0900

    regexp-re-explain: add -f flag, pattern from file

--- cmd/regexp-re-explain/main.go
+++ cmd/regexp-re-explain/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -42,20 +43,43 @@ func (m *memoryFlag) String() string {
 
 func main() {
 	var maxMemory memoryFlag = memoryFlag(ir.MaxDFAMemory)
+	patternFile := flag.String("f", "", "Read pattern from file")
+
 	flag.Var(&maxMemory, "m", "Max memory limit for DFA construction (e.g., 64m, 1g)")
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: regexp-re-explain [-m memory_limit] \"{pattern}\"\n")
+		fmt.Fprintf(os.Stderr, "Usage: regexp-re-explain [-m memory_limit] [-f file] [pattern]\n")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 
-	args := flag.Args()
-	if len(args) != 1 {
+	var pattern string
+	if *patternFile != "" {
+		b, err := os.ReadFile(*patternFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading pattern file: %v\n", err)
+			os.Exit(1)
+		}
+		pattern = string(b)
+	} else if args := flag.Args(); len(args) > 0 {
+		pattern = args[0]
+	} else {
+		// Read from STDIN
+		b, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading pattern from STDIN: %v\n", err)
+			os.Exit(1)
+		}
+		pattern = string(b)
+	}
+
+	// Trim trailing newline if present (common in files and piped input)
+	pattern = strings.TrimRight(pattern, "\n")
+
+	if pattern == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	pattern := args[0]
 	re, err := regexp.CompileWithOptions(pattern, regexp.CompileOptions{
 		MaxMemory: int(maxMemory),
 	})
